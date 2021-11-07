090e21d3e5f1cdb00ae21f24a590609d9a5178a3
Markus Jankowski <markus@mm-jankowski.de>
2021-11-07 11:21:38 +0100

    deactivate hub

--- custom_components/hahm/__init__.py
+++ custom_components/hahm/__init__.py
@@ -104,7 +104,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.entry_id] = cu
     hass.config_entries.async_setup_platforms(entry, HA_PLATFORMS)
     await cu.start()
-    await hass.async_add_executor_job(cu.init_hub)
+    #await hass.async_add_executor_job(cu.init_hub)
     #await async_setup_services(hass)
     return True
 
--- custom_components/hahm/controlunit.py
+++ custom_components/hahm/controlunit.py
@@ -74,6 +74,9 @@ class ControlUnit:
         """Start the server."""
         _LOGGER.debug("Starting HAHM ControlUnit %s", self._data[ATTR_INSTANCE_NAME])
         config.CACHE_DIR = "cache"
+        config.TIMEOUT = 10
+        config.PROXY_EXECUTOR_MAX_WORKERS = 1
+        config.JSON_EXECUTOR_MAX_WORKERS = 4
 
         self.create_server()
 
