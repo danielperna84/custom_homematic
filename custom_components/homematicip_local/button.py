bd3ffecac28c08023abeb784bfb884d699521c7c
SukramJ <sukramj@icloud.com>
2022-08-02 19:30:25 +0200

    Add program buttons

--- custom_components/homematicip_local/button.py
+++ custom_components/homematicip_local/button.py
@@ -5,7 +5,7 @@ import logging
 from typing import Any
 
 from hahomematic.const import HmPlatform
-from hahomematic.platforms.button import HmButton
+from hahomematic.platforms.button import HmButton, HmProgramButton
 
 from homeassistant.components.button import ButtonEntity
 from homeassistant.config_entries import ConfigEntry
@@ -13,9 +13,9 @@ from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import DOMAIN
+from .const import ATTR_NAME, DOMAIN
 from .control_unit import ControlUnit
-from .generic_entity import HaHomematicGenericEntity
+from .generic_entity import HaHomematicGenericEntity, HaHomematicGenericHubEntity
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -39,6 +39,17 @@ async def async_setup_entry(
         if entities:
             async_add_entities(entities)
 
+    @callback
+    def async_add_program_button(args: Any) -> None:
+        """Add program button from Homematic(IP) Local."""
+        entities: list[HaHomematicProgramButton] = []
+
+        for hm_entity in args:
+            entities.append(HaHomematicProgramButton(control_unit, hm_entity))
+
+        if entities:
+            async_add_entities(entities)
+
     config_entry.async_on_unload(
         async_dispatcher_connect(
             hass,
@@ -49,8 +60,24 @@ async def async_setup_entry(
         )
     )
 
+    config_entry.async_on_unload(
+        async_dispatcher_connect(
+            hass,
+            control_unit.async_signal_new_hm_entity(
+                config_entry.entry_id, HmPlatform.HUB_BUTTON
+            ),
+            async_add_program_button,
+        )
+    )
+
     async_add_button(
         control_unit.async_get_new_hm_entities_by_platform(platform=HmPlatform.BUTTON)
+    )
+
+    async_add_program_button(
+        control_unit.async_get_new_hm_hub_entities_by_platform(
+            platform=HmPlatform.HUB_BUTTON
+        )
     )
 
 
@@ -60,3 +87,28 @@ class HaHomematicButton(HaHomematicGenericEntity[HmButton], ButtonEntity):
     async def async_press(self) -> None:
         """Execute a button press."""
         await self._hm_entity.press()
+
+
+class HaHomematicProgramButton(HaHomematicGenericHubEntity, ButtonEntity):
+    """Representation of the Homematic button."""
+
+    def __init__(
+        self,
+        control_unit: ControlUnit,
+        hm_program_button: HmProgramButton,
+    ) -> None:
+        """Initialize the button entity."""
+        super().__init__(
+            control_unit=control_unit,
+            hm_hub_entity=hm_program_button,
+        )
+        self._hm_hub_entity: HmProgramButton = hm_program_button
+
+    @property
+    def extra_state_attributes(self) -> dict[str, Any]:
+        """Return the state attributes of the program button entity."""
+        return {ATTR_NAME: self._hm_hub_entity.ccu_program_name}
+
+    async def async_press(self) -> None:
+        """Execute a button press."""
+        await self._hm_hub_entity.press()
