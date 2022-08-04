19e6bac594d3c829935a50c05db47cd8ec8c0dce
SukramJ <sukramj@icloud.com>
2022-08-04 21:23:30 +0200

    Update __init__.py

--- custom_components/homematicip_local/__init__.py
+++ custom_components/homematicip_local/__init__.py
@@ -22,10 +22,10 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     """Set up HA-Homematic from a config entry."""
     if HA_VERSION_OBJ < HMIP_LOCAL_MIN_VERSION:
         _LOGGER.warning(
-            "This release of HomematicIP Local requires HA version %s and above",
+            "This release of Homematic(IP) Local requires HA version %s and above",
             HMIP_LOCAL_MIN_VERSION,
         )
-        _LOGGER.warning("HomematicIP Local setup blocked")
+        _LOGGER.warning("Homematic(IP) Local setup blocked")
         return False
     control = await ControlConfig(
         hass=hass,
