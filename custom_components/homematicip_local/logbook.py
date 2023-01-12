76d3fa44171a8c847b97f282bbad2d707d8761f1
SukramJ <sukramj@icloud.com>
2023-01-12 12:47:03 +0100

    Bump hahomematic to 2023.1.3 (#391)
    * Bump hahomematic to 2023.1.3
    
    * Reassign event parameters in control_unit

--- custom_components/homematicip_local/logbook.py
+++ custom_components/homematicip_local/logbook.py
@@ -4,14 +4,14 @@ from __future__ import annotations
 
 from collections.abc import Callable
 
-from hahomematic.const import HmEventType
-from hahomematic.entity import BaseEvent
+from hahomematic.const import ATTR_ADDRESS, ATTR_CHANNEL_NO, ATTR_PARAMETER, HmEventType
+from hahomematic.entity import GenericEvent
 
 from homeassistant.components.logbook import LOGBOOK_ENTRY_MESSAGE, LOGBOOK_ENTRY_NAME
-from homeassistant.const import ATTR_DEVICE_ID, CONF_ADDRESS, CONF_TYPE
+from homeassistant.const import ATTR_DEVICE_ID, CONF_TYPE
 from homeassistant.core import Event, HomeAssistant, callback
 
-from .const import CONF_SUBTYPE, DOMAIN as HMIP_DOMAIN
+from .const import CONF_SUBTYPE, DOMAIN as HMIP_DOMAIN, EVENT_DATA_ERROR_VALUE
 from .control_unit import get_device
 
 
@@ -23,14 +23,16 @@ def async_describe_events(
     """Describe logbook events."""
 
     @callback
-    def async_describe_homematic_event(event: Event) -> dict[str, str]:
-        """Describe Homematic(IP) Local logbook event."""
+    def async_describe_homematic_click_event(event: Event) -> dict[str, str]:
+        """Describe Homematic(IP) Local logbook click event."""
         event_data: dict = event.data
 
         if hm_device := get_device(hass=hass, device_id=event.data[ATTR_DEVICE_ID]):
-            channel_address = f"{event_data[CONF_ADDRESS]}:{event_data[CONF_SUBTYPE]}"
+            channel_address = f"{event_data[ATTR_ADDRESS]}:{event_data[CONF_SUBTYPE]}"
             e_type = event_data[CONF_TYPE].upper()
-            hm_event: BaseEvent | None = hm_device.events.get((channel_address, e_type))
+            hm_event: GenericEvent | None = hm_device.events.get(
+                (channel_address, e_type)
+            )
             if hm_event:
                 event_name = hm_event.full_name.replace(
                     hm_event.parameter.replace("_", " ").title(), ""
@@ -44,11 +46,11 @@ def async_describe_events(
     async_describe_event(
         HMIP_DOMAIN,
         HmEventType.KEYPRESS.value,
-        async_describe_homematic_event,
+        async_describe_homematic_click_event,
     )
 
     async_describe_event(
         HMIP_DOMAIN,
         HmEventType.IMPULSE.value,
-        async_describe_homematic_event,
+        async_describe_homematic_click_event,
     )
