3dcf229b4ac7d954ea7f71c5a93fcbceb3e48c9d
SukramJ <sukramj@icloud.com>
2023-01-27 08:05:34 +0100

    align api

--- custom_components/homematicip_local/climate.py
+++ custom_components/homematicip_local/climate.py
@@ -266,10 +266,10 @@ class HaHomematicClimate(
 
     async def async_set_hvac_mode(self, hvac_mode: HVACMode) -> None:
         """Set new target hvac mode."""
-        if hvac_mode in HA_TO_HM_HVAC_MODE:
-            await self._hm_entity.set_hvac_mode(HA_TO_HM_HVAC_MODE[hvac_mode])
-        else:
+        if hvac_mode not in HA_TO_HM_HVAC_MODE:
             _LOGGER.warning("Hvac mode %s is not supported by integration", hvac_mode)
+            return None
+        await self._hm_entity.set_hvac_mode(HA_TO_HM_HVAC_MODE[hvac_mode])
 
     async def async_set_preset_mode(self, preset_mode: str) -> None:
         """Set new preset mode."""
