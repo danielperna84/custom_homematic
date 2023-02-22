e1a864b336d2fab5b0a02d6016ab58e1bb53affe
SukramJ <sukramj@icloud.com>
2023-02-22 10:23:24 +0100

    Enable HmIP-SMSD as siren.

--- custom_components/homematicip_local/siren.py
+++ custom_components/homematicip_local/siren.py
@@ -5,7 +5,7 @@ import logging
 from typing import Any
 
 from hahomematic.const import HmPlatform
-from hahomematic.platforms.custom.siren import BaseSiren, CeIpSiren, HmSirenArgs
+from hahomematic.platforms.custom.siren import BaseSiren, HmSirenArgs
 import voluptuous as vol
 
 from homeassistant.components.siren import (
@@ -52,8 +52,7 @@ async def async_setup_entry(
         entities: list[HaHomematicGenericRestoreEntity] = []
 
         for hm_entity in args:
-            if isinstance(hm_entity, CeIpSiren):
-                entities.append(HaHomematicSiren(control_unit, hm_entity))
+            entities.append(HaHomematicSiren(control_unit, hm_entity))
 
         if entities:
             async_add_entities(entities)
