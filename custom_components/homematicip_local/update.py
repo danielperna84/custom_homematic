f51a8c89b16484e9503bcb8a80e0c41919348f00
SukramJ <sukramj@icloud.com>
2023-06-01 09:58:22 +0200

    Fix latest version of update entity (#464)

--- custom_components/homematicip_local/update.py
+++ custom_components/homematicip_local/update.py
@@ -139,7 +139,11 @@ class HaHomematicUpdate(UpdateEntity):
             HmDeviceFirmwareState.PERFORMING_UPDATE,
         ):
             return self._hm_entity.available_firmware
-        return None
+        return (
+            self._hm_entity.available_firmware
+            if self._hm_entity.available_firmware
+            else self._hm_entity.firmware
+        )
 
     @property
     def name(self) -> str | None:
