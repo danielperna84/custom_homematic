cf1edacc12f0b0f4c9b49e171a9444ab04b1b875
SukramJ <sukramj@icloud.com>
2023-08-29 21:42:15 +0200

    Readd attribute const to recorder

--- custom_components/homematicip_local/recorder.py
+++ custom_components/homematicip_local/recorder.py
@@ -3,17 +3,28 @@ from __future__ import annotations
 
 from homeassistant.core import HomeAssistant, callback
 
+from .generic_entity import (
+    ATTR_ADDRESS,
+    ATTR_ENTITY_TYPE,
+    ATTR_FUNCTION,
+    ATTR_INTERFACE_ID,
+    ATTR_MODEL,
+    ATTR_NAME,
+    ATTR_PARAMETER,
+    ATTR_VALUE_STATE,
+)
+
 
 @callback
 def exclude_attributes(hass: HomeAssistant) -> set[str]:
     """Exclude static attributes from being recorded in the database."""
     return {
-        "address"
-        "entity_type",
-        "function",
-        "interface_id",
-        "model",
-        "name",
-        "parameter",
-        "value_state",
+        ATTR_ADDRESS,
+        ATTR_ENTITY_TYPE,
+        ATTR_FUNCTION,
+        ATTR_INTERFACE_ID,
+        ATTR_MODEL,
+        ATTR_NAME,
+        ATTR_PARAMETER,
+        ATTR_VALUE_STATE,
     }
